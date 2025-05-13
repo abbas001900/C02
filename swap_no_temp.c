3951cbd4c9c33a63f5ac86f0a67014cac97c891b
Abbas Ismael <aismoctar@gmail.com>
2025-05-13 15:46:56 +0100

    Create swap_no_temp.c

---
+++ swap_no_temp.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+    int a, b;
+
+    // Lecture des deux nombres
+    scanf("%d %d", &a, &b);
+
+
+    // Échange sans utiliser de variable temporaire
+    a = a + b;  // a devient la somme de a et b
+    b = a - b;  // b devient l'ancienne valeur de a
+    a = a - b;  // a devient l'ancienne valeur de b
+
+    // Affichage après échange
+    printf("Après échange: a = %d, b = %d\n", a, b);
+
+    return 0;
+}
