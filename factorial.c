4c92dfd8b7874f72f9bf0bc45bd9fb59c8a0fe7a
Abbas Ismael <aismoctar@gmail.com>
2025-05-13 15:42:15 +0100

    Update factorial.c

--- factorial.c
+++ factorial.c
@@ -4,14 +4,13 @@ int main(void) {
     int nbre;
     int factorial = 1;
 
-    printf("Entrez un nombre entier: ");
-    scanf("%d", &n);
+    scanf("%d", &nbre);
 
     for (int i = 1; i <= nbre; i++) {
         factorial *= i;
     }
 
-    printf("Le factoriel de %d est : %d\n", n, factorial);
+    printf("Le factoriel de %d est : %d\n", nbre, factorial);
 
     return 0;
 }
