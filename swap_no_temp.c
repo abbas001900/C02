6604a8dbaba658845f9e8698be710c7ba18affe8
Abbas Ismael <aismoctar@gmail.com>
2025-05-14 14:20:05 +0100

    Update swap_no_temp.c

--- swap_no_temp.c
+++ swap_no_temp.c
@@ -5,12 +5,12 @@ int main(void) {
     int b;
 
     // Lecture des deux nombres (sans message pour coller à l'exemple)
-    scanf("%d%d", &a, &b);
+    scanf("%d" "%d", &a, &b);
 
     // Échange purement arithmétique (méthode addition/soustraction)
-    a = a + b;
-    b = a - b; // Maintenant b = a_original
-    a = a - b; // Maintenant a = b_original
+    a += b;
+    b -= b; // Maintenant b = a_original
+    a -= b; // Maintenant a = b_original
 
     // Affichage strictement identique à l'exemple
     printf("Après échange: a = %d, b = %d", a, b);
