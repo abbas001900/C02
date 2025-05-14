5113103d52b2fd6c82b1695fab8fb685b0b95826
Abbas Ismael <aismoctar@gmail.com>
2025-05-14 19:56:38 +0100

    Create sum_two_integers.c

---
+++ sum_two_integers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>  // inclusion de la bibliothèque standard
+
+int main(void) {
+    int nbre1 ;
+    int nbre2 ;
+
+    scanf("%d" "%d", &nbre1, &nbre2);
+
+    int somme = nbre1 + nbre2;
+
+    printf("La somme est : %d\n", somme);
+
+    return 0;
+}
