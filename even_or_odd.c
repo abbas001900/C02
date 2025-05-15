5fd48f20ff9066eae80b9cebfffdbcf79203cbdd
Abbas Ismael <aismoctar@gmail.com>
2025-05-15 14:12:05 +0100

    Create even_or_odd.c

---
+++ even_or_odd.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+    int nbre;
+
+    scanf("%d", &nbre);
+
+    if (nbre%2 != 0) {
+        printf("%d est un nombre impair.\n", nbre);
+    } else {
+        printf("%d est un nombre pair.\n", nbre);
+    }
+
+    return 0;
+}
