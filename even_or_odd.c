4f8de716a6afb79f84b6b392a142a6be0f7b1fc8
Abbas Ismael <aismoctar@gmail.com>
2025-05-15 19:38:14 +0100

    Update even_or_odd.c

--- even_or_odd.c
+++ even_or_odd.c
@@ -6,15 +6,12 @@ int main(void) {
 
     scanf("%d", &nbre); // Demande a l'utilisateur d'entrer un nombre et assignation de la valeur a la variable nombre
     cond = nbre % 2;
+    
     // Si le restant de la division du nombre entre par l'utlisateur par 2 est 0
-    if (cond == 0) {
-        printf("%d est un nombre pair.", nbre); // Ecrire nbre (le nombre entre par l'utilisateur) est un nombre pair
-    }
-
-    else {
+    if(cond == 0)
+        printf("%d est un nombre pair.\n", nbre); // Ecrire nbre (le nombre entre par l'utilisateur) est un nombre pair
+    else
         printf("%d est un nombre impair.", nbre); // Sinon Ecrire nbre(le nombre entre par l'utilisateur) est un nombre impair
-        
-    }
 
     return 0;
 }
