e365689df3a85a13f565a766edd0f38acd1e49be
Abbas Ismael <aismoctar@gmail.com>
2025-05-16 14:37:27 +0100

    Update swap_no_temp.c

--- swap_no_temp.c
+++ swap_no_temp.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 
 int main(void) {
-    int a;
-    int b;
+    int a, b;
 
     scanf("%d %d", &a, &b);
 
+    // Échange sans variable temporaire
     a = a + b;
     b = a - b;
     a = a - b;
-
 
     printf("Après échange: a = %d, b = %d\n", a, b);
 
