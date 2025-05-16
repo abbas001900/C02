8c45ae6102e0e442513c777c45b3827872fcb2f4
Abbas Ismael <aismoctar@gmail.com>
2025-05-16 14:41:47 +0100

    test

---
+++ even_or_odd.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int nombre;
+    int resultat;
+
+    // scanf retourne 1 si la lecture d'un entier réussit
+    scanf("%d", &nombre);
+
+    if (nombre % 2 == 0) {
+        printf("%d est pair.\n", nombre);
+    } else {
+        printf("%d est impair.\n", nombre);
+
+    return 0;
+}
+}
