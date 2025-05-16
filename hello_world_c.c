99358aece8d7c9db828570dc5432a2ec796ea3c2
Abbas Ismael <aismoctar@gmail.com>
2025-05-16 14:38:44 +0100

    test

--- hello_world_c.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>  // inclusion de la bibliothèque standard
-
-int main(void) {
-    printf("Hello, World!\n");  // affichage du message "Hello, World!"
-    return 0;  // fin du programme
-}
