737263c9f3bca0156f73efe1414f56c61c1c4ce4
Abbas Ismael <aismoctar@gmail.com>
2025-05-16 08:07:49 +0100

    Update even_or_odd.c

--- even_or_odd.c
+++ even_or_odd.c
@@ -3,11 +3,18 @@
 int main(void) {
     int nbre;
     int cond;
-    scanf("%d", &nbre);
+    int retour;
     
+    retour = scanf("%d", &nbre);
     cond = nbre % 2;
 
-    (cond == 0) ? printf("%d est un nombre pair.\n", nbre) : printf("%d est un nombre impair.\n", nbre);
+    if (retour == 1) {
+        (cond == 0) ? printf("%d est un nombre pair.\n", nbre) : printf("%d est un nombre impair.\n", nbre);
+    }
+    else {
+        printf("Veuillez entrer un entier.\n");
+    }
+
 
     return 0;
 }
