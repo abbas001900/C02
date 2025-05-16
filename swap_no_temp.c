d6f1d09594ddd52e3f165de835cfaabd9f8e4009
Abbas Ismael <aismoctar@gmail.com>
2025-05-16 14:39:48 +0100

    Create swap_no_temp.c

---
+++ swap_no_temp.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+    int a, b;
+
+    scanf("%d %d", &a, &b);
+
+    // Échange sans variable temporaire
+    a = a + b;
+    b = a - b;
+    a = a - b;
+
+    printf("Après échange: a = %d, b = %d\n", a, b);
+
+    return 0;
+}
